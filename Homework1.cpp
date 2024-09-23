f3b8caa8ce329add83c03241c560a10bd25b685f
ShadeOfMorr <shcheka.d@gmail.com>
2024-09-23 11:47:51 +0300

    Update Homework1.cpp

--- Homework1.cpp
+++ Homework1.cpp
@@ -1,8 +1,12 @@
 /* Nikita Kotegov st128906@student.spbu.ru Homework1 */
+// Этот файл должен называться main.cpp
 
 #include <iostream>
 #include "Homework1.hpp"
 
+ /* На самом деле так делать нехорошо, потому что это загрязняет
+ * пространство имен и иногда провоцирует конфликты имет, поэтому 
+ * привыкай просто везде писать std:: */
 using namespace std;
 
 int main(int argc, char *argv[]) {
