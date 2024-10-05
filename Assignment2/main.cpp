ed7e78bcf59bfa4dade694916baf9a1ffc5d7ffd
KotegovNikita <st128906@student.spbu.ru>
2024-10-05 14:25:23 +0300

    Create array, read the file in array and close infile

--- Assignment2/main.cpp
+++ Assignment2/main.cpp
@@ -7,5 +7,12 @@ int main() {
     infile.open("file.jpg", std::ios::binary);
 
     int fileSize = std::filesystem::file_size("file.jpg");
+
+    char* buffer = new char[fileSize];
+
+    infile.read(buffer, fileSize);
+
+    infile.close();
+
     return 0;
 }
