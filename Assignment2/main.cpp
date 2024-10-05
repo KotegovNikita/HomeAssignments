ba674357048be5caf5b38cf62af6b778af739923
KotegovNikita <st128906@student.spbu.ru>
2024-10-05 14:39:13 +0300

    Added a lopp which expand array

--- Assignment2/main.cpp
+++ Assignment2/main.cpp
@@ -12,6 +12,12 @@ int main() {
 
     infile.read(buffer, fileSize);
 
+    for(std::int i = 0; i < fileSize / 2; i++) {
+        char t = buffer[i];
+        buffer[i] = buffer[fileSize - i - 1];
+        buffer[fileSize - i - 1] = t;
+    }
+
     infile.close();
 
     return 0;
