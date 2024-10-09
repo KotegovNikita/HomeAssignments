c1782db2f52084022fb6a2177e240122cb648c0a
KotegovNikita <st128906@student.spbu.ru>
2024-10-09 19:54:42 +0300

    Replacing includes with a header

--- Assignment2/main_2b.cpp
+++ Assignment2/main_2b.cpp
@@ -1,12 +1,32 @@
 /* Nikita Kotegov st128906@student.spbu.ru Group B81 Assignment_2b */
 
-#include "header_2b"
+#include "header_2b.hpp"
 
-int* stack;
-int top = -1;
-int capacity;
+
+int operation_num(char operation, int num1, int num2) { //Function for proccessing operations on numbers
+    switch(operation) {
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case '*':
+            return num1 * num2;
+        case '/':
+            return num1 / num2;
+        default:
+            return 1;
+    }
+}
+
+void push(int value) { // Function for add value in stack
+    stack[++top] = value;
+}
+int pop() { // Function for extraction value from stack
+    return stack[top--];
+}
 
 int main() {
+    
     std::string input_string = "3 4 + 2 * 7 /"; // String on input
 
     capacity = input_string.length();
