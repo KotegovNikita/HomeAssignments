a364a1e7ba6cab2b3a99797f41e60cec73658192
KotegovNikita <st128906@student.spbu.ru>
2024-10-09 19:41:12 +0300

    Create header file for main_2a

---
+++ Assignment2/header_2a.hpp
@@ -0,0 +1,9 @@
+/* Nikita Kotegov st128906@student.spbu.ru Group B81 Assignment_2a */
+
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+
+#endif
