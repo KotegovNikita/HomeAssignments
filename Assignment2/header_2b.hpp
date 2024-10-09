0e48d2c9ed6ce8d8bdbcc1e840c47a0f81b82490
KotegovNikita <st128906@student.spbu.ru>
2024-10-09 18:54:27 +0300

    Create header file for main_2b

---
+++ Assignment2/header_2b.hpp
@@ -0,0 +1,12 @@
+/* Nikita Kotegov st128906@student.spbu.ru Group B81 Assignment_2b */
+
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include <iostream>
+#include <string>
+
+int operation_num(char operation, int num1, int num2);
+void push(int value);
+int pop();
+
+#endif
