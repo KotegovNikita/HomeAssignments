d36e8e019e83868a8ddc900b5495c68f808a7df8
KotegovNikita <st128906@student.spbu.ru>
2024-10-12 21:39:02 +0300

    Delete Assignment2

--- Assignment2/header_2a.hpp
+++
@@ -1,9 +0,0 @@
-/* Nikita Kotegov st128906@student.spbu.ru Group B81 Assignment_2a */
-
-#ifndef HEADER_FILE
-#define HEADER_FILE
-#include <iostream>
-#include <fstream>
-#include <filesystem>
-
-#endif
