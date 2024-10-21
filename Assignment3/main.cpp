1e7ea1e536a589a2850a80d7d0ce624be6cfd171
KotegovNikita <st128906@student.spbu.ru>
2024-10-21 22:16:29 +0300

    Add classes Decepticon and neutral

--- Assignment3/main.cpp
+++ Assignment3/main.cpp
@@ -51,13 +51,13 @@ public:
 class Autobot: public Transformers {
 private:
     std::string hostility;
-    int helth;
+    int health;
 public:
     Autobot(std::string n, int p, int a, Alliance* al, std::string hostility)
         : Transformers(n, p, a, Type("Autobot"), al), hostility(hostility), health(100) {}
 
     std::string getHostility() const {
-        retutn hostility;
+        return hostility;
     }
     void setHostility(const std::string& h) {
         hostility = h;
@@ -66,7 +66,46 @@ public:
     int getHealth() const {
         return health;
     }
-    void setHealth(int h) {
-    health = he;
+    void setHealth(int he) {
+        health = he;
+    }
+};
+
+
+class Decepticon: public Transformers {
+private:
+    std::string hostility;
+    int health;
+public:
+    Decepticon(std::string n, int p, int a, Alliance* al, std::string hostility)
+        : Transformers(n, p, a, Type("Decepticon"), al), hostility(hostility), health(100) {}
+
+    std::string getHostility() const {
+        return hostility;
+    }
+    void setHostility(const std::string& h) {
+        hostility = h;
+    }
+
+    int getHealth() const {
+        return health;
+    }
+    void setHealth(int he) {
+        health = he;
+    }
+};
+
+class Neutral: public Transformers {
+private:
+    std::string purpose;
+public:
+    Neutral(std::string n, int p, int a, Alliance* al, std::string purpose)
+        : Transformers(n, p, a, Type("Neutral"), al), purpose(purpose) {}
+
+    std::string getPurpose() const {
+        return purpose;
+    }
+    void setPurpose(const std::string& p) {
+        purpose = p;
     }
 };
