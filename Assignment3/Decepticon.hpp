fec63b940368e876c979b6e8fde55995c5580f69
KotegovNikita <st128906@student.spbu.ru>
2024-10-22 10:58:24 +0300

    Header file for Decepticon.cpp

---
+++ Assignment3/Decepticon.hpp
@@ -0,0 +1,19 @@
+#ifndef DECEPTICON_HPP
+#define DECEPTICON_HPP
+
+#include <string>
+#include "Transformers.hpp"
+
+class Decepticon : public Transformers {
+private:
+    std::string hostility;
+    int health;
+public:
+    Decepticon(std::string n, int p, int a, Alliance* al, std::string hostility);
+    std::string getHostility() const;
+    void setHostility(const std::string& h);
+    int getHealth() const;
+    void setHealth(int he);
+};
+
+#endif
