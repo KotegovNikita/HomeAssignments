d5d6d6fae24d907377889dd2f3aac89b5948968b
KotegovNikita <st128906@student.spbu.ru>
2024-10-22 10:56:55 +0300

    Header file to Autobot.cpp

---
+++ Assignment3/Autobot.hpp
@@ -0,0 +1,19 @@
+#ifndef AUTOBOT_HPP
+#define AUTOBOT_HPP
+
+#include <string>
+#include "Transformers.hpp"
+
+class Autobot : public Transformers {
+private:
+    std::string hostility;
+    int health;
+public:
+    Autobot(std::string n, int p, int a, Alliance* al, std::string hostility);
+    std::string getHostility() const;
+    void setHostility(const std::string& h);
+    int getHealth() const;
+    void setHealth(int he);
+};
+
+#endif
