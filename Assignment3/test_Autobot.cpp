110d02cd4edf811d3735071f5402a93e4885217d
KotegovNikita <st128906@student.spbu.ru>
2024-10-24 23:06:37 +0300

    Transferring the call of all tests to a separate filr

--- Assignment3/test_Autobot.cpp
+++ Assignment3/test_Autobot.cpp
@@ -38,8 +38,4 @@ TEST(AutobotTest, HealthTest) {
     EXPECT_EQ(autobot.getHealth(), 50);
 }
 
-// Точка входа для Google Test
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
+
