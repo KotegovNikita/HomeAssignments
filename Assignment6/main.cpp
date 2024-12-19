3dae5b258602b5a2e9aefd8d48e3d236405cfac1
Kotegov Nikita <st128906@student.spbu.ru>
2024-12-20 01:56:47 +0300

    Add changes to correct work

--- Assignment6/main.cpp
+++ Assignment6/main.cpp
@@ -1,24 +1,146 @@
 #include "main.hpp"
 
-int main() {
-    Class1 class1;
-    MyClass<Class1> myClass1(class1);
-    std::cout << "MyClass<Class1>::foo() = " << myClass1.foo() << std::endl;
+template <typename T1, typename T2>
+MyTClass<T1, T2>::MyTClass(int _num, const std::vector<float>& _v, const T1& _obj1, const T2& _obj2)
+    : number(_num), vec(_v), inst1(_obj1), inst2(_obj2) {}
 
-    Class2 class2;
-    MyClass<Class2> myClass2(class2);
-    std::cout << "MyClass<Class2>::foo() = " << myClass2.foo() << std::endl;
+template <typename T1, typename T2>
+bool MyTClass<T1, T2>::foo() {
+    return inst1.bar(number, vec);
+}
 
-    Class3 class3;
-    MyClass<Class3> myClass3(class3);
-    std::cout << "MyClass<Class3>::foo() = " << myClass3.foo() << std::endl;
+template<typename T>
+MyTClass<int, T>::MyTClass(int _num, const std::vector<float>& _v, int _inst1, const T& _obj)
+    : number(_num), vec(_v), inst1(_inst1), inst2(_obj) {}
 
-    MyClass<int> myIntClass(5);
-    std::cout << "MyClass<int>::foo() = " << myIntClass.foo() << std::endl;
+template<typename T>
+bool MyTClass<int, T>::foo() {
+    return true;
+}
 
-    MyClass<double> myDoubleClass(3.14);
-    std::cout << "MyClass<double>::foo() = " << myDoubleClass.foo() << std::endl;
+template<typename T>
+MyTClass<double, T>::MyTClass(int _num, const std::vector<float>& _v, double _inst1, const T& _obj)
+    : number(_num), vec(_v), inst1(_inst1), inst2(_obj) {}
 
-    return 0;
+template<typename T>
+bool MyTClass<double, T>::foo() {
+    return false;
+}
+
+bool Class1::bar(int num, const std::vector<float>& vec) {
+    return num > 0;
+}
+
+int Class1::c_1_1() {
+    std::cout << "c_1_1\n";
+    return 1;
+}
+
+float Class1::c_1_2() {
+    std::cout << "c_1_2\n";
+    return 0.1;
+}
+
+void Class1::c_1_3() {
+    std::cout << "c_1_3\n";
+}
+
+bool Class2::bar(int num, const std::vector<float>& vec) {
+    return !vec.empty();
+}
+
+int Class2::c_2_1() {
+    std::cout << "c_2_1\n";
+    return 2;
+}
+
+float Class2::c_2_2() {
+    std::cout << "c_2_2\n";
+    return 0.2;
+}
+
+void Class2::c_2_3() {
+    std::cout << "c_2_3\n";
+}
+
+bool Class3::bar(int num, const std::vector<float>& vec) {
+    return num == -int(vec.size());
+}
+
+int Class3::c_3_1() {
+    std::cout << "c_3_1\n";
+    return 3;
+}
+
+float Class3::c_3_2() {
+    std::cout << "c_3_2\n";
+    return 0.3;
+}
+
+void Class3::c_3_3() {
+    std::cout << "c_3_3\n";
+}
+
+MyTClass<Class1, int> MyTClassTest::createClass1Test(int num, const std::vector<float>& vec, int inst1) {
+    return MyTClass<Class1, int>(num, vec, cl1, inst1);
+}
+
+MyTClass<Class2, int> MyTClassTest::createClass2Test(int num, const std::vector<float>& vec, int inst1) {
+    return MyTClass<Class2, int>(num, vec, cl2, inst1);
+}
+
+MyTClass<Class3, int> MyTClassTest::createClass3Test(int num, const std::vector<float>& vec, int inst1) {
+    return MyTClass<Class3, int>(num, vec, cl3, inst1);
+}
+
+TEST_F(MyTClassTest, Class1FooReturnsTrue) {
+    std::vector<float> vec = {1.0f, 2.0f, 3.0f};
+    auto myClass1 = createClass1Test(5, vec, 10);
+    EXPECT_TRUE(myClass1.foo());
+}
+
+TEST_F(MyTClassTest, Class1FooReturnsFalse) {
+    std::vector<float> vec = {1.0f, 2.0f, 3.0f};
+    auto myClass1 = createClass1Test(-5, vec, 10);
+    EXPECT_FALSE(myClass1.foo());
+}
+
+TEST_F(MyTClassTest, Class2FooReturnsTrue) {
+    std::vector<float> vec = {1.0f, 2.0f, 3.0f};
+    auto myClass2 = createClass2Test(1, vec, 10);
+    EXPECT_TRUE(myClass2.foo());
+}
+
+TEST_F(MyTClassTest, Class2FooReturnsFalse) {
+    std::vector<float> vec = {};
+    auto myClass2 = createClass2Test(-5, vec, 10);
+    EXPECT_FALSE(myClass2.foo());
+}
+
+TEST_F(MyTClassTest, Class3FooReturnsTrue) {
+    std::vector<float> vec = {1.0f, 2.0f, 3.0f};
+    auto myClass3 = createClass3Test(-3, vec, 10);
+    EXPECT_TRUE(myClass3.foo());
+}
+
+TEST_F(MyTClassTest, Class3FooReturnsFalse) {
+    std::vector<float> vec = {1.0f, 2.0f, 3.0f};
+    auto myClass3 = createClass3Test(3, vec, 10);
+    EXPECT_FALSE(myClass3.foo());
+}
+
+TEST_F(MyTClassTest, IntSpecializationReturnsTrue) {
+    MyTClass<int, Class1> myIntClass(0, {}, 0, cl1);
+    EXPECT_TRUE(myIntClass.foo());
+}
+
+TEST_F(MyTClassTest, DoubleSpecializationReturnsFalse) {
+    MyTClass<double, Class1> myDoubleClass(0, {}, 0.0, cl1);
+    EXPECT_FALSE(myDoubleClass.foo());
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
 
--- Assignment6/main.hpp
+++ Assignment6/main.hpp
@@ -1,77 +1,80 @@
 #ifndef MAIN_HPP
 #define MAIN_HPP
 
-#include <iostream>
 #include <vector>
+#include <iostream>
+#include <gtest/gtest.h>
 
-template <typename T>
-class MyClass {
-public:
-    MyClass(T obj) : obj_(obj) {}
-
-    bool foo() {
-        return obj_.bar(number_, vec_);
-    }
-
-    int number_ = 5;
-    std::vector<float> vec_ = {1.0, 2.0, 3.0};
-
+template <typename T1, typename T2>
+class MyTClass {
 private:
-    T obj_;
+    int number;
+    std::vector<float> vec;
+    T1 inst1;
+    T2 inst2;
+public:
+    MyTClass(int _num, const std::vector<float>& _v, const T1& _obj1, const T2& _obj2);
+    bool foo();
 };
 
-template <>
-class MyClass<int> {
-public:
-    MyClass(int obj) : obj_(obj) {}
-
-    bool foo() {
-        return true;
-    }
-
+template<typename T>
+class MyTClass<int, T> {
 private:
-    int obj_;
+    int number;
+    std::vector<float> vec;
+    int inst1;
+    T inst2;
+public:
+    MyTClass(int _num, const std::vector<float>& _v, int _inst1, const T& _obj);
+    bool foo();
 };
 
-template <>
-class MyClass<double> {
-public:
-    MyClass(double obj) : obj_(obj) {}
-    
-    bool foo() {
-        return false;
-    }
+template<typename T>
+class MyTClass<double, T> {
 private:
-    double obj_;
+    int number;
+    std::vector<float> vec;
+    double inst1;
+    T inst2;
+public:
+    MyTClass(int _num, const std::vector<float>& _v, double _inst1, const T& _obj);
+    bool foo();
 };
 
 class Class1 {
 public:
-    bool bar(int x, std::vector<float>&) {
-        return x > 0;
-    }
-    int c_i_1() { return 1; }
-    float c_i_2() { return 1.1f; }
-    void c_i_3() {}
+    bool bar(int num, const std::vector<float>& vec);
+    int c_1_1();
+    float c_1_2();
+    void c_1_3();
 };
 
 class Class2 {
 public:
-    bool bar(int, std::vector<float>& vec) {
-        return !vec.empty();
-    }
-    int c_i_1() { return 2; }
-    float c_i_2() { return 2.2f; }
-    void c_i_3() {}
+    bool bar(int num, const std::vector<float>& vec);
+    int c_2_1();
+    float c_2_2();
+    void c_2_3();
 };
 
 class Class3 {
 public:
-    bool bar(int x, std::vector<float>& vec) {
-        return x == -static_cast<int>(vec.size());
-    }
-    int c_i_1() { return 3; }
-    float c_i_2() { return 3.3f; }
-    void c_i_3() {}
+    bool bar(int num, const std::vector<float>& vec);
+    int c_3_1();
+    float c_3_2();
+    void c_3_3();
 };
+
+class MyTClassTest : public ::testing::Test {
+public:
+    Class1 cl1;
+    Class2 cl2;
+    Class3 cl3;
+
+    MyTClass<Class1, int> createClass1Test(int num, const std::vector<float>& vec, int inst1);
+    MyTClass<Class2, int> createClass2Test(int num, const std::vector<float>& vec, int inst1);
+    MyTClass<Class3, int> createClass3Test(int num, const std::vector<float>& vec, int inst1);
+};
+
 #endif
+
